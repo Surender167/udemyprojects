94ce41c1e029813ede1e07aa4980e94d6230a181
Surender167 <Surenderreddy71@gmail.com>
2022-01-27 18:57:01 +0530

    commited

---
+++ src/Udemytrilas/Str.java
@@ -0,0 +1,18 @@
+package Udemytrilas;
+
+public class Str {
+
+	public static void main(java.lang.String[] args) {
+		
+		String s1 =  "S";
+		String s2 = "Surender";
+		String s3 = "Reddy";
+// for giving the space use double quotation with space
+		String s4 = s1+ " Sure " +s2+s3;
+		System.out.println(s4);
+		
+		//TODO Auto-generated method stub
+ 
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author suren
+ *
+ */
+module Udemypractice {
+}
